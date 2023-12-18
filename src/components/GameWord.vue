3a22c7dcfc88e2130ad425de501bfbf4b650a0cc
Arseny Prosvetov <arsprosvet@gmail.com>
2023-12-18 13:40:53 +0300

    Update GameWord

--- src/components/GameWord.vue
+++ src/components/GameWord.vue
@@ -1,16 +1,50 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+
 interface Props {
-  word: string
-  correctLetters: string[]
+  word: string;
+  correctLetters: string[];
+  onUpdateLetter: (letter: string) => void;
+  isDisplayInput: boolean;
 }
 
-defineProps<Props>()
+const { word, correctLetters, onUpdateLetter, isDisplayInput } =
+  defineProps<Props>();
+
+const inputLetter = ref<string>("");
+
+const onInput = () => {
+  if (inputLetter.value.length === 1) {
+    onUpdateLetter(inputLetter.value);
+    inputLetter.value = "";
+  }
+};
+
+const isMobileOrTablet = ref<boolean>(false);
+
+onMounted(() => {
+  isMobileOrTablet.value =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    );
+});
 </script>
 
 <template>
   <div class="word">
     <span v-for="(letter, index) in word" :key="index" class="letter">
-      {{ correctLetters.includes(letter) ? letter : '' }}
+      {{ correctLetters.includes(letter) ? letter : "" }}
     </span>
+
+    <div v-if="isMobileOrTablet && isDisplayInput" class="input-container">
+      <input
+        type="text"
+        :value="inputLetter"
+        @input="onInput"
+        maxlength="1"
+        placeholder="'ж'"
+        class="input"
+      />
+    </div>
   </div>
 </template>
