1b027b758d4e3fcdf5922f2437ad6286df31fcb9
Arseny Prosvetov <arsprosvet@gmail.com>
2023-12-18 13:50:53 +0300

    Update App

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,81 @@
 <script setup lang="ts">
-import GameHeader from './components/GameHeader.vue'
-import GameFigure from './components/GameFigure.vue'
-import GameWrongLetters from './components/GameWrongLetters.vue'
-import GameWord from './components/GameWord.vue'
-import GamePopap from './components/GamePopap.vue'
-import GameNotification from './components/GameNotification.vue'
-import { ref, watch } from 'vue'
-import { useRandomWord } from './composables/useRandomWord'
-import { useLetters } from './composables/useLetters'
-import { useNotification } from './composables/useNotification'
+import GameHeader from "./components/GameHeader.vue";
+import GameFigure from "./components/GameFigure.vue";
+import GameWrongLetters from "./components/GameWrongLetters.vue";
+import GameWord from "./components/GameWord.vue";
+import GamePopap from "./components/GamePopap.vue";
+import GameNotification from "./components/GameNotification.vue";
+import { ref, watch } from "vue";
+import { useRandomWord } from "./composables/useRandomWord";
+import { useLetters } from "./composables/useLetters";
+import { useNotification } from "./composables/useNotification";
 
-const { word, getRandomWord } = useRandomWord()
-const { letters, correctLetters, wrongLetters, isLose, isWin, addLetter, resetLetters } =
-  useLetters(word)
-const { notification, showNotification } = useNotification()
-const popup = ref<InstanceType<typeof GamePopap> | null>(null)
+const { word, getRandomWord } = useRandomWord();
+const {
+  letters,
+  correctLetters,
+  wrongLetters,
+  isLose,
+  isWin,
+  addLetter,
+  resetLetters,
+} = useLetters(word);
+const handleUpdateLetter = (letter: string) => {
+  // Обработка введенной буквы
+  console.log(`Введена буква: ${letter}`);
+};
+const { notification, showNotification } = useNotification();
+const popup = ref<InstanceType<typeof GamePopap> | null>(null);
 const restart = async () => {
-  await getRandomWord()
-  resetLetters()
-  popup.value?.close()
-}
+  await getRandomWord();
+  resetLetters();
+  popup.value?.close();
+  isDisplayInput.value = true;
+};
+const isDisplayInput = ref<boolean>(true);
 
 watch(wrongLetters, () => {
   if (isLose.value) {
-    popup.value?.open('lose')
+    popup.value?.open("lose");
+    isDisplayInput.value = false;
   }
-})
+});
 
 watch(correctLetters, () => {
   if (isWin.value) {
-    popup.value?.open('win')
+    popup.value?.open("win");
+    isDisplayInput.value = false;
   }
-})
+});
 
-window.addEventListener('keydown', ({ key }) => {
+window.addEventListener("keydown", ({ key }) => {
+  const lowerCaseKey = key.toLowerCase();
+
   if (isLose.value || isWin.value) {
-    return
+    return;
   }
 
-  if (letters.value.includes(key)) {
-    showNotification()
-    return
+  if (letters.value.includes(lowerCaseKey)) {
+    showNotification();
+    return;
   }
 
-  addLetter(key)
-})
+  addLetter(lowerCaseKey);
+});
 </script>
 
 <template>
   <GameHeader />
   <div class="game-container">
     <GameFigure :wrong-letters-count="wrongLetters.length" />
+    <GameWord
+      :on-update-letter="handleUpdateLetter"
+      :word="word"
+      :correct-letters="correctLetters"
+      :is-display-input="isDisplayInput"
+    />
     <GameWrongLetters :wrong-letters="wrongLetters" />
-    <GameWord :word="word" :correct-letters="correctLetters" />
   </div>
-
   <GamePopap ref="popup" :word="word" @restart="restart" />
   <GameNotification ref="notification" />
 </template>
