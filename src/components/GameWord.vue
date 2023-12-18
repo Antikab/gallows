9e068a99d8ec73ba4844eda342a964a737bb8210
Arseny Prosvetov <arsprosvet@gmail.com>
2023-12-18 16:59:16 +0300

    Update GameWord add mobile

--- src/components/GameWord.vue
+++ src/components/GameWord.vue
@@ -24,9 +24,9 @@ const isMobileOrTablet = ref<boolean>(false);
 
 onMounted(() => {
   isMobileOrTablet.value =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent,
-    );
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    window.matchMedia("(hover: none)").matches;
 });
 </script>
 
@@ -42,7 +42,57 @@ onMounted(() => {
         :value="inputLetter"
         @input="onInput"
         maxlength="1"
-        placeholder="'ж'"
+        placeholder="'а'"
+        class="input"
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+interface Props {
+  word: string;
+  correctLetters: string[];
+  addLetter: (letter: string) => void;
+  isDisplayInput: boolean;
+}
+
+const { word, correctLetters, addLetter, isDisplayInput } =
+  defineProps<Props>();
+
+const inputLetter = ref<string>("");
+const maxInputLength = 1;
+const onInput = () => {
+  if (inputLetter.value.length === maxInputLength) {
+    addLetter(inputLetter.value);
+    inputLetter.value = "";
+  }
+};
+
+const isMobileOrTablet = ref<boolean>(false);
+
+onMounted(() => {
+  isMobileOrTablet.value =
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    window.matchMedia("(hover: none)").matches;
+});
+</script>
+
+<template>
+  <div class="word">
+    <span v-for="(letter, index) in word" :key="index" class="letter">
+      {{ correctLetters.includes(letter) ? letter : "" }}
+    </span>
+
+    <div v-if="isMobileOrTablet && isDisplayInput" class="input-container">
+      <input
+        type="text"
+        :value="inputLetter"
+        @input="onInput"
+        maxlength="1"
+        placeholder="'а'"
         class="input"
       />
     </div>
